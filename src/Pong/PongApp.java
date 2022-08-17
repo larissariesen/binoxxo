69d84e15cc12f0de990cc6191447a961f9db78be
Michael Job <mj@michaeljob.ch>
2022-08-17 09:00:42 +0200

    Win (#1)
    * win
    
    * win and mac fix
    
    Co-authored-by: MichaelJob <6egsvjdFBo6SHvJg6egf>

--- src/Pong/PongApp.java
+++ src/Pong/PongApp.java
@@ -2,11 +2,16 @@ package Pong;
 
 import processing.core.PApplet;
 
+
 //Source https://gist.github.com/dc74089/4094da7928839063ae06
 public class PongApp extends PApplet {
     float x, y, speedX, speedY;
     float diam = 20;
     float rectSize = 80;
+
+    public static void main(String args[]) {
+        PApplet.main(new String[] { PongApp.class.getName() });
+    }
 
     public void settings() {
         size(640, 360);
